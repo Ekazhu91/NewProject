17ffae90ae6d2ddf7018b9df0a0aa57200575990
Ekazhu91 <zhuravskaya_e@inbox.ru>
2023-06-15 11:11:32 +0300

    Законченная 2-ая практика

--- login/urls.py
+++ login/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from .views import IndexView
+from .views import IndexView, LogoutView, CreateAccountView
 
+app_name = 'login'
 
 urlpatterns = [
-   path('', IndexView.as_view())
+   path('', IndexView.as_view(), name="login"),
+   path('logout/', LogoutView.as_view(), name="logout"),
+   path('create/', CreateAccountView.as_view(), name="create"),
 ]
--- store/views.py
+++ store/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.views import View
+from django.db.models import OuterRef, Subquery, F, ExpressionWrapper, DecimalField, Case, When
+from django.utils import timezone
+from store.models import Product
+
 
 # Create your views here.
 
@@ -75,6 +79,17 @@ class ProductView(View):
                 }
         return render(request, 'store/product-single.html', context=data[id])
 
+    def get_(self, request, id):
+        data = Product.objects.get(id=id)
+        return render(request,
+                      "store/product-single.html",
+                      context={'name': data.name,
+                               'description': data.description,
+                               'price': data.price,
+                               'rating': 5.0,
+                               'url': data.image.url,
+                               })
+
 
 class ShopView(View):
     def get(self, request):
@@ -144,3 +159,32 @@ class ShopView(View):
                             ]
                    }
         return render(request, 'store/shop.html', context)
+
+    # Создание запроса на получения всех действующих не нулевых скидок
+    def get_(self, request):
+        discount_value = Case(When(discount__value__gte=0,
+                                   discount__date_begin__lte=timezone.now(),
+                                   discount__date_end__gte=timezone.now(),
+                                   then=F('discount__value')),
+                              default=0,
+                              output_field=DecimalField(max_digits=10, decimal_places=2)
+                              )
+        # Создание запроса на расчёт цены со скидкой
+        price_with_discount = ExpressionWrapper(
+            F('price') * (100.0 - F('discount_value')) / 100.0,
+            output_field=DecimalField(max_digits=10, decimal_places=2)
+        )
+
+        products = Product.objects.annotate(
+            discount_value=discount_value,
+            # Другой способ через запрос в другую таблицу, однако
+            # без фильтрации по времени действия скидки
+            # discount_value=Subquery(
+            #     Discount.objects.filter(product_id=OuterRef('id')).values(
+            #         'value')
+            # ),
+            price_before=F('price'),
+            price_after=price_with_discount
+        ).values('id', 'name', 'image', 'price_before', 'price_after',
+                 'discount_value')
+        return render(request, 'store/shop.html', {"data": products})
